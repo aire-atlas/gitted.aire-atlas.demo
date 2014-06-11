563051594b2707f63ef9712703e1729caa825f1b
jf <jf@geonef.fr>
2014-06-11 14:46:33 +0200

    work on install.sh

--- install.sh
+++ install.sh
@@ -6,8 +6,8 @@ _aire_install()
 
     _packages="$_packages apache2-mpm-prefork libapache2-mod-php5"
     _packages="$_packages postgresql-9.1 postgresql-9.1-postgis"
-    _packages="$_packages mapserver-bin cgi-mapserver"
-    _packages="$_packages php5-cli"
+    _packages="$_packages mapserver-bin cgi-mapserver php5-mapscript"
+    _packages="$_packages php5-cli php-pear"
 
 
     # utilities
@@ -23,6 +23,25 @@ _aire_install()
     sysconf_apt-get install --no-upgrade $_packages
 
     rm -f /etc/apache2/sites-enabled/000-default
+
+    [ -d /usr/share/javascript/openlayers-2.11rc3 ] || {
+        mkdir -p /usr/share/javascript/openlayers-2.11rc3 || nef_fatal "could not mkdir"
+        curl "http://openlayers.org/download/OpenLayers-2.11-rc3.tar.gz" \
+            | tar xzv --strip-components=1 -C /usr/share/javascript/openlayers-2.11rc3 \
+            || nef_fatal "could not download or extract OpenLayers archive"
+    }
+    # [ -d /usr/share/javascript/dojo-release-1.5 ] || {
+    #     mkdir -p /usr/share/javascript/dojo-release-1.5
+
+    #     http://download.dojotoolkit.org/release-1.5.3/dojo-release-1.5.3-src.tar.gz
+    # [ -d /usr/share/javascript/dojo-release-1.5.0-patched-fc262d0d ] || {
+    #     # mkdir -p /usr/share/javascript/dojo-release-1.5.0-patched-fc262d0d
+    #     curl "http://download.dojotoolkit.org/release-1.5.0/dojo-release-1.5.0-src.tar.gz" \
+    #         | tar xzv --strip-components=1 -C /usr/share/javascript/dojo-release-1.5.0-patched-fc262d0d \
+    #         || nef_fatal "could not download or extract Dojo archive"
+    #     curl https://raw.githubusercontent.com/dojo/dojo/fc262d0d589c490cdd671791f1546a4665ed69c6/selector/acme.js \
+    #         > /usr/share/javascript/dojo-release-1.5.0-patched-fc262d0d
+    # }
 }
 
 _aire_setup()
@@ -30,6 +49,17 @@ _aire_setup()
     grep -q ^aire: /etc/passwd || {
         useradd -d / aire
     }
+
+    _aire_enable_apache_module headers.load
+    _aire_enable_apache_module rewrite.load
+    _aire_enable_apache_module expires.load
+}
+
+_aire_enable_apache_module()
+{
+    moduleName=$1
+    [ -h /etc/apache2/mods-enabled/$moduleName ] || \
+        ln -s ../mods-available/$moduleName /etc/apache2/mods-enabled/
 }
 
 _aire_reload()
