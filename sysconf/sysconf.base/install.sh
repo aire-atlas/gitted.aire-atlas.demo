c8caa56882c0f6a1114c43c497f07e18879063c3
okapi <jf@geonef.fr>
2014-08-21 02:03:44 +0200

    Add 'glxc/sysconf/sysconf.base/' from commit 'ec87bfe781acf8192c7ec8fa21fc656a9f3c381c'
    git-subtree-dir: glxc/sysconf/sysconf.base
    git-subtree-mainline: 6967dea924e4a2f89cc5869f77c31a1719bcaea8
    git-subtree-split: ec87bfe781acf8192c7ec8fa21fc656a9f3c381c

---
+++ sysconf/sysconf.base/install.sh
@@ -0,0 +1,43 @@
+# sysconf install script for conf: "base"
+
+
+
+######################################################################
+#
+# Set up /etc/bash.bashrc to source all /etc/bash.bashrc.d/*.sh
+
+bashrcfile=/etc/bash.bashrc
+bashrcdir=/etc/bash.bashrc.d
+
+grep -q $bashrcdir $bashrcfile || cat >>$bashrcfile <<EOF
+
+# Include *.sh files from $bashrcdir
+# (added by $0 on $(date))
+if [ -d $bashrcdir ]; then
+  for i in $bashrcdir/*.sh; do
+    if [ -r $i ]; then
+      . \$i
+    fi
+  done
+  unset i
+fi
+EOF
+
+######################################################################
+#
+# Set up /etc/network/interfaces to source all /etc/network/interfaces.d/*.interfaces
+
+[ -d /etc/network/interfaces.d ] && {
+    cat /etc/network/interfaces | grep -q "source /etc/network/interfaces.d" || {
+        echo "Fixing /etc/network/interfaces for sourcing interfaces.d/"
+        echo >>/etc/network/interfaces
+        echo 'source /etc/network/interfaces.d/*.interfaces' >>/etc/network/interfaces
+    }
+}
+
+######################################################################
+#
+# Same for /etc/hosts, /etc/ssh/config, /etc/gitconfig
+nef-update-hosts
+nef-update-ssh-config
+nef-update-gitconfig
