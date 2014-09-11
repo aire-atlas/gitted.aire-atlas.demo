81e3d29747edd91e48f95eb6c331a9b9ebb3f866
okapi <jf@geonef.fr>
2014-09-11 04:13:31 +0200

    moved gitted/sysconf to .

--- README.subtrees.md
+++
@@ -1,1 +0,0 @@
-git subtree push -P gitted/sysconf/sysconf.gitted git@github.com:geonef/sysconf.gitted.git gitted
--- aire-data/public/help/inc/learning.php
+++
@@ -1,8 +0,0 @@
-<?php
-// FRANCAIS
-$title_fr ="E-learning";
-$content_fr = '(not disponible)';
-
-// ANGLAIS
-$title_en ='E-learning';
-$content_en = '(not available yet)';
